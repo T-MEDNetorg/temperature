0b829d44df834abf135c3bfbc9f4f0e5124687ed
Marc <marcjb2@gmail.com>
2025-10-23 11:38:09 +0200

    Added functionality to database_reader to be able to write the information gathered in df into excel files to be properly used for the reports.

--- reporter/main_reporter.py
+++ reporter/main_reporter.py
@@ -5,4 +5,6 @@ top10_max, top10_min, top10_max_year, top10_min_year = DataBase.create_top_tens(
 
 DataBase.create_mhw_dict()
 
+DataBase.create_excel([top10_max, top10_max_year, top10_min, top10_min_year], "./tops.xlsx")
+DataBase.create_excel(DataBase.dict_mhw, "./mhws.xlsx")
 print('hey')
